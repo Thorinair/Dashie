909dfd91dbbd37cd9d3d50d6671ff4b1fa8049cf
Thorinair <thorinair@yahoo.com>
2019-09-30 18:50:07 +0200

    Updated Luna example config.

--- ConfigurationLuna_EXAMPLE.h
+++ ConfigurationLuna_EXAMPLE.h
@@ -9,9 +9,6 @@
 /* Princess Luna API key. */
 #define LUNA_KEY "aaaaaaaaaaaaaaaa0000000000000000"
 
-/* URL called to dump sensor data. */
-#define LUNA_URL_DEBUG "/?action=celly"
-
 /* URL called to dump boot data. */
 #define LUNA_URL_BOOT  "/?action=boot"
 
